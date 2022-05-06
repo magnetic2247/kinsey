f087c3b4cc584c5440cdee1100157fdc92759543
Peanut Butter <Polis Minus>
2022-05-06 16:42:53 +0400

    cleaned it up and removed hardcoded config path

--- main.cpp
+++ main.cpp
@@ -1,58 +1,72 @@
 #include <boost/filesystem.hpp>
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 #include <string>
 #include <ctime>
-#include <cstdlib>
 
 using namespace std;
 namespace fs = boost::filesystem;
 
+/*
+Name: Kinsey
+Author: Polis Minus
+License: GPLv3
+Description: Logs a user's Kinsey Scale rating daily
+*/
+
 int main() {
+    // Get XDG Config directory
+    string config;
+    if (getenv("XDG_CONFIG_HOME") == NULL) {
+        config = getenv("HOME");
+        config = config+"/.config";
+    }
+    else config = getenv("XDG_CONFIG_HOME");
 
-  // Read the last saved epoch 
-  // Stop if already answered within the last 24 hours
-  // Basically if current_epoch - last_epoch < 86400
-  ifstream epoch_file;
-  epoch_file.open("/home/minus/.config/kinseyscale/last_epoch");
-  string last_epoch_str;
-  getline(epoch_file, last_epoch_str);
-  int last_epoch = stoi(last_epoch_str);
-  if (time(0)-last_epoch < 86400) return 0;
+    // Create config directory if needed
+    // Create epoch file if empty
+    if (!fs::is_directory(config+"/kinseyscale/")) fs::create_directory(config+"/kinseyscale/");
+    fstream epoch_file;
+    epoch_file.open(config+"/kinseyscale/last_epoch", ios::in | ios::out);
+    epoch_file.seekg(0, epoch_file.end);
+    if (epoch_file.tellg() == 0) epoch_file << "0" << endl;
 
-  // THIS CODE IS AWFUL! I'm gonna rewrite it all.
+    // Read the last saved epoch 
+    // Stop if already answered within the last 24 hours
+    // Basically if current_epoch - last_epoch < 86400
+    string last_epoch_str;
+    epoch_file.seekg(0, ios::beg);
+    getline(epoch_file, last_epoch_str);
+    int last_epoch = stoi(last_epoch_str);
+    if (time(0)-last_epoch < 86400) { // Less than 24 hours since last epoch, quit
+        epoch_file.close();
+        return 0;
+    }
 
-  // Kinsey Scale Rating
-  int kinsey = -1;
-  
-  // Repeat the question until input is valid
-  // The Kinsey Scale ranges from 0 to 6
-  do {
-    cout << "Daily Kinsey Scale Rating: ";
-    cin >> kinsey;
-  } while (kinsey < 0 || kinsey > 7);
+    // Open CSV File
+    // Write CSV headers if file is empty
+    fstream csv_file;
+    csv_file.open(config+"/kinseyscale/data.csv", ios::app | ios::ate);
+    if (csv_file.tellg() == 0) csv_file << "epoch,kinsey" << endl;
 
-  // Find Home Directory
-  string home = getenv("HOME");
-  // Create XDG config if needed
-  if (!fs::is_directory(home+"/.config/kinseyscale")) fs::create_directory(home+"/.config/kinseyscale");
+    // User Input
+    // Repeat the question until input is valid
+    // The Kinsey Scale ranges from 0 to 6
+    int kinsey = -1;
+    do {
+        cout << "Daily Kinsey Scale Rating: ";
+        cin >> kinsey;
+    } while (kinsey < 0 || kinsey > 7);
 
-  // Open CSV File
-  fstream file;
-  file.open(home+"/.config/kinseyscale/data.csv", ios::app | ios::ate);
+    // Write data to csv file + last epoch
+    csv_file << time(0) << "," << kinsey << endl;
+    epoch_file.seekg(0, ios::beg);
+    epoch_file << time(0);
 
-  // Write CSV headers if file is empty
-  if (file.tellg() == 0) file << "epoch,kinsey" << endl;
-  // Write CSV Data
-  file << time(0) << "," << kinsey << endl;
+    // Close Files
+    epoch_file.close();
+    csv_file.close();
 
-  // Write epoch to date file
-  file.close();
-  file.open(home+"/.config/kinseyscale/last_epoch", ios::out |  ios::trunc);
-  file << time(0);
-  file.close();
-  
-
-  return 0;
+    return 0;
 }
-
